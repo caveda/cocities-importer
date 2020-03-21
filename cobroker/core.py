7ad2d0362aae183ffcfa8ff740338506c5e63bb8
Victor Caveda <caveda@gmail.com>
2020-03-21 18:17:10 +0100

    Format issues

--- cobroker/core.py
+++ cobroker/core.py
@@ -1,20 +1,12 @@
 import requests
-import cobroker.cocities
 from cobroker import cocities
-import re
-from xml.dom import minidom
-
-from cobroker.model import Line, LINE_FORWARD_DIRECTION
 from cobroker.parser_lines import parse_lines
 from cobroker.parser_routes import parse_route
 from cobroker.parser_stops import parse_stops
 
-"""
-    Returns the complete list of lines without stops or routes.
-"""
-
 
 def get_all_lines():
+    """ Returns the complete list of lines without stops or routes. """
     query = cocities.get_request_all_lines()
     req = send_http_request(query)
     req.encoding = 'utf-8'  # force utf-8 encoding to preserve special chars
@@ -22,12 +14,8 @@ def get_all_lines():
     return lines
 
 
-"""
-    Returns the stops of the given line.
-"""
-
-
 def get_line_stops(line):
+    """ Returns the stops of the given line."""
     query = cocities.get_request_line_stops(line.id)
     req = send_http_request(query)
     stops = parse_stops(req.text, line)
