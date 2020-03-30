7278b81eb726775e816f5316b39afd2911c6a0db
Victor Caveda <caveda@gmail.com>
2020-03-30 18:17:37 +0200

    Lines sorted as part of sanitize op

--- codata_linter.py
+++ codata_linter.py
@@ -1,4 +1,12 @@
-
-def sanitize (lines):
+def sanitize(lines):
     """ Receives a list of lines, removes the faulty ones and returns a sanitized copy. """
+    filtered_lines = remove_lines_without_stops(lines)
+    return sort_lines_by_id(filtered_lines)
+
+
+def sort_lines_by_id(lines):
+    return sorted(lines, key=lambda line: line.id)
+
+
+def remove_lines_without_stops(lines):
     return filter(lambda line: len(line.stops) > 0, lines)
